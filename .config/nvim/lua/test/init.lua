67e4078088a3f271a8aa5833a615e42656218912
MAITRECRAFT <victor.jurgens-mestre@epitech.eu>
2024-02-03 23:54:07 +0100

    [ADD] base config files

---
+++ .config/nvim/lua/test/init.lua
@@ -0,0 +1,11 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require("test.packer")
+require("test.keybinds")
+require("test.set")
+print("loaded test")
