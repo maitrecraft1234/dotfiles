3042605e8ca1c16f52bed07ff5fd86d5ea1e3d56
MAITRECRAFT <105306215+maitrecraft1234@users.noreply.github.com>
2024-09-05 14:22:43 +0200

    fix the up select going 2 lines up and whatnot

--- .config/nvim/lua/test/keybinds.lua
+++ .config/nvim/lua/test/keybinds.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " " --first key to be pressed for these keymaps
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-3<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -9,10 +9,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
